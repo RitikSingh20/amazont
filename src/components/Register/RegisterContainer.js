1e5275dd23cca2363f8de926fa012ce6354b73a4
ritik723 <64574373+ritik723@users.noreply.github.com>
2021-02-23 08:51:48 +0530

    Update RegisterContainer.js

--- src/components/Register/RegisterContainer.js
+++ src/components/Register/RegisterContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import RegisterView from './RegisterView'
-import React from './react'
+ 
 
 class RegisterContainer extends Component {
   constructor(props){
